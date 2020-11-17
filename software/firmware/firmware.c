301ae2dbafa3620719382e614700c1cc61463673
Dr-Specimen <jose.j.sarmento@me.com>
2020-11-17 21:45:40 +0000

    added random-gen-max lookup

--- software/firmware/firmware.c
+++ software/firmware/firmware.c
@@ -1,30 +1,28 @@
 #include "system.h"
 #include "periphs.h"
 #include "iob-uart.h"
-#include "stdint.h"
+#include <stdio.h> 
+#include <stdlib.h>
+#include <time.h>
 
 int main()
-{ 
-  int n = 100; // Fibonacci sequence numbers
-  unsigned long f_lsd[n], f_msd[n];
-  
-  int i;
-  
-  f_lsd[0] = 0; f_lsd[1] = 1;
-  for(i = 2; i < n; i++){
-      f_lsd[i] = f_lsd[i-1] + f_lsd[i-2];
-  }
-  
+{
+  int n=100;
+  int i, max;
+  int random_number[100];
+  srand(time(NULL));
+
   //init uart 
   uart_init(UART_BASE,FREQ/BAUD);   
   uart_printf("\n\n\nHello world!\n\n\n");
-  uart_printf("First %d numbers of the Fibonacci sequence:\n\n\n", n);
 
-for(i = 46; i < n; i++){
-    if(f_msd[i]==0)
-	uart_printf("nº%d:\t%d\n", i+1, f_lsd[i]);
-    else
-	uart_printf("nº%d:\t%d%d\n", i+1,f_msd[i], f_lsd[i]);
- }
+  random_number[0]=rand();
+  for(i=1; i<n; i++){
+      random_number[i]=rand();
+      if(random_number[i]>random_number[i-1])
+	  max = random_number[i];
+  }
+
+  uart_printf("Max number is: %d\n\n", max);
 
 }
